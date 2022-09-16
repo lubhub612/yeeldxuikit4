8eced644d5738e0f765c5b8b052fc50ce4931a6c
lubhub612 <lundhakgithub@gmail.com>
2022-09-17 02:26:05 +0530

    first commit

---
+++ src/widgets/Menu/theme.ts
@@ -0,0 +1,15 @@
+import { darkColors, lightColors } from "../../theme/colors";
+
+export interface NavThemeType {
+  background: string;
+}
+
+export const light: NavThemeType = {
+ // background: lightColors.backgroundAlt,
+ background: lightColors.violetAlt1,
+};
+
+export const dark: NavThemeType = {
+//  background: darkColors.backgroundAlt,
+background: darkColors.violetAlt1,
+};
